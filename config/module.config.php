45a3a70f8495892d675600add6514b337f2b2981
Alexandru Dobre <alexandru.dobre@webtales.fr>
2016-10-25 13:50:26 +0200

    WIP base theme port

--- config/module.config.php
+++ config/module.config.php
@@ -1,4 +1,24 @@
 <?php
 return array(
-
+    'templates' => array(
+        'themes' => array(
+            'demoperso' => array(
+                'label' => 'Demoperso',
+                'basePath' => realpath(__DIR__ . '/../themes/demoperso'),
+                'noBootstrap'=>true,
+                'css' => array(
+                    "/css/bootstrap.min.css",
+                    "/css/font-awesome.min.css",
+                    "/css/prettyPhoto.css",
+                    "/css/animate.css",
+                    "/css/main.css",
+                    "https://fonts.googleapis.com/css?family=Raleway"
+                ),
+                'js' => array(
+                    "/js/bootstrap.min.js",
+                    "/js/jquery.prettyPhoto.js"
+                )
+            ),
+        )
+    ),
 );
