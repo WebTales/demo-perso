c95312b06bf39aab789d48450daf909c33c58cde
Alexandru Dobre <alexandru.dobre@webtales.fr>
2016-10-26 10:55:55 +0200

    directive

--- themes/demoperso/js/blockDefinitions.js
+++ themes/demoperso/js/blockDefinitions.js
@@ -7,3 +7,36 @@ blocksConfig.multiCarrousel={
     "internalDependencies":["/src/modules/rubedoBlocks/controllers/MultiCarrouselController.js"],
     "externalDependencies":['/components/OwlFonk/OwlCarousel/owl-carousel/owl.carousel.min.js']
 };
+angular.module("rubedoBlocks").directive('persoContentLink',["RubedoContentsService","RubedoPagesService",function (RubedoContentsService,RubedoPagesService) {
+    return {
+        link: function (scope, element, attrs) {
+
+            var options = {
+                siteId: scope.rubedo.current.site.id,
+                pageId: scope.rubedo.current.page.id
+            };
+            var contentId=attrs.content;
+            RubedoPagesService.getPageById(attrs.page).then(function(response){
+                if (response.data.success){
+                    var destinationPage=response.data.url;
+                    if (contentId&&contentId!=""){
+                        RubedoContentsService.getContentById(contentId, options).then(
+                            function(response){
+                                if (response.data.success){
+                                    console.log(response.data);
+                                    var cannonical=response.data.content.canonicalUrl;
+                                    var cutUrl=cannonical.substring(cannonical.indexOf(response.data.content.id));
+                                    attrs.$set("href",destinationPage+"/"+cutUrl);
+                                    element.html(response.data.content.fields.text);
+                                }
+                            }
+                        );
+                    }
+                }
+            });
+
+
+
+        }
+    };
+}]);
